d85d6b6d47040348e48399399f29f70f45817c16
Chirag Singh <130963807+chiragsingh9123@users.noreply.github.com>
2024-07-30 14:16:29 +0530

    Update upload2.py

--- upload2.py
+++ upload2.py
@@ -2,8 +2,8 @@ import shutil
 import os
 
 def create_folder_and_upload(local_file_path1):
-        source_folder = f'../ai2api_bot/{local_file_path1}'
-        destination_folder = '../../var/www/atlanta-api.online/scripts'
+        source_folder = f'../peytas/{local_file_path1}'
+        destination_folder = '/var/www/sourceotp.online/scripts'
         if not os.path.exists(destination_folder+f"/{local_file_path1}"):
             shutil.move(source_folder, destination_folder)
             print(f"Successfully uploaded {local_file_path1} to {destination_folder}")
